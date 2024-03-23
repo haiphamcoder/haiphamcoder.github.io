36910fc7cb1ff02d33a5e55abda33c4a02839c01
Hai Pham Ngoc <ngochai285nd@gmail.com>
2024-03-23 13:56:49 +0700

    reformat code and add skill image ubuntu

--- next.config.js
+++ next.config.js
@@ -6,3 +6,14 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// const path = require('path')
+ 
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     domains: ['res.cloudinary.com', 'media.dev.to']
+//   }
+// }
